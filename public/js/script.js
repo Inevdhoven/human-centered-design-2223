7463d01286edbb3b4ad72faa461e6a3825d0e435
Inevdhoven <inevdhoven@gmail.com>
2023-04-20 11:13:09 +0200

    Made some changes

--- public/js/script.js
+++ public/js/script.js
@@ -57,82 +57,39 @@ const snap = document.getElementById('snap');
 const cameraBtn = document.getElementById('camera-btn');
 const cameraContainer = document.getElementById('camera-container');
 
+const constraints = {
+    video: true
+};
+
 async function startCamera() {
     try {
-        const constraints = {
-            video: {
-                facingMode: 'environment' // Gebruik "user" om de frontcamera te gebruiken
-            }
-        };
-        const stream = await navigator.mediaDevices.getUserMedia(constraints); // Vraag toestemming voor de camera
-        handleSuccess(stream); // Als de gebruiker toestemming geeft, start de camera
-        cameraContainer.style.display = 'block'; // Laat de camera zien
-        cameraBtn.style.display = 'none'; // Verberg de knop om de camera te starten
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+        cameraContainer.style.display = 'block';
+        video.classList.add('active'); // Voeg class "active" toe aan video
+        canvas.classList.add('active'); // Voeg class "active" toe aan canvas
+        cameraBtn.style.display = 'none';
+        video.play(); // Start het afspelen van de video
     } catch (e) {
         console.error('Error: ', e);
     }
 }
 
 function handleSuccess(stream) {
-    window.stream = stream; // Zorg ervoor dat de stream beschikbaar is in de console
-    video.srcObject = stream; // Laat de video zien
+    window.stream = stream;
+    video.srcObject = stream;
 }
 
 snap.addEventListener("click", function () {
-    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height); // Maak een foto van de video
+    canvas.getContext("2d").drawImage(video, 0, 0, 640, 480);
 });
 
 cameraBtn.addEventListener("click", function () {
-    startCamera(); // Start de camera
+    startCamera();
 });
 
-// Pas de grootte van de video- en canvas-elementen aan op basis van het apparaat
-function adjustVideoSize() {
-    const videoRatio = video.videoWidth / video.videoHeight;
-    const containerWidth = cameraContainer.offsetWidth;
-    const containerHeight = cameraContainer.offsetHeight;
-    const containerRatio = containerWidth / containerHeight;
+// Voeg eventlistener toe aan video-element om afspelen te triggeren bij gebruikersinteractie
+video.addEventListener('click', function () {
+    video.play();
+});
 
-    if (videoRatio > containerRatio) {
-        video.width = containerWidth;
-        video.height = containerWidth / videoRatio;
-    } else {
-        video.width = containerHeight * videoRatio;
-        video.height = containerHeight;
-    }
-    canvas.width = video.width;
-    canvas.height = video.height;
-}
-
-// Voer de aanpassing van de video- en canvas-grootte uit bij het laden van de pagina en bij het draaien van het apparaat
-adjustVideoSize();
-window.addEventListener('resize', adjustVideoSize);
-window.addEventListener('orientationchange', adjustVideoSize);
-
-// const constraints = {
-//     video: true
-// };
-
-// async function startCamera() {
-//     try {
-//         const stream = await navigator.mediaDevices.getUserMedia(constraints);
-//         handleSuccess(stream);
-//         cameraContainer.style.display = 'block';
-//         cameraBtn.style.display = 'none';
-//     } catch (e) {
-//         console.error('Error: ', e);
-//     }
-// }
-
-// function handleSuccess(stream) {
-//     window.stream = stream;
-//     video.srcObject = stream;
-// }
-
-// snap.addEventListener("click", function () {
-//     canvas.getContext("2d").drawImage(video, 0, 0, 640, 480);
-// });
-
-// cameraBtn.addEventListener("click", function () {
-//     startCamera();
-// });
