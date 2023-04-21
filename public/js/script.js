ce03b19596afb59d58404ee6d15764e6d45b3eaa
Inevdhoven <inevdhoven@gmail.com>
2023-04-21 12:49:12 +0200

    Added close button and send button

--- public/js/script.js
+++ public/js/script.js
@@ -46,21 +46,56 @@ socket.on('typing', (inputName) => {
     }, 3000);
 })
 
+socket.on('image', (data) => {
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    img.src = data.image;
+    li.appendChild(img);
+    li.innerHTML += data.name;
+
+    // Check if the message is sent by the user
+    if (data.name === inputName.value) {
+        li.classList.add('current-user');
+    }
+
+    messages.appendChild(li);
+    typingState.innerHTML = "";
+    messages.scrollTop = messages.scrollHeight
+});
+
 function addMessage(data) {
     messages.appendChild(Object.assign(document.createElement('li'), { textContent: data.name + ': ' + data.message }))
     messages.scrollTop = messages.scrollHeight
+}
+
+function addImage(data) {
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    img.src = data.image;
+    li.appendChild(img);
+    li.innerHTML += data.name;
+
+    // Check if the message is sent by the user
+    if (data.name === inputName.value) {
+        li.classList.add('current-user');
+    }
+
+    messages.appendChild(li);
 }
 
 
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
 // Define constants
-const cameraBtn = document.querySelector('#camera-btn');
 const camera = document.querySelector('#camera');
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    cameraBtn = document.querySelector('#camera-btn'),
+    cameraCloseBtn = document.querySelector('#camera--close'),
+    cameraUploadBtn = document.querySelector('#camera--upload');
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -72,11 +107,23 @@ function cameraStart() {
             cameraView.play();
             cameraView.classList.add("active");
             cameraTrigger.classList.add("active");
+            cameraCloseBtn.classList.add("active");
+            cameraUploadBtn.classList.add("active");
         })
         .catch(function (error) {
             console.error("Oops. Something is broken.", error);
         });
 }
+
+cameraCloseBtn.addEventListener("click", function () {
+    camera.classList.remove("active");
+    cameraView.classList.remove("active");
+    cameraTrigger.classList.remove("active");
+    cameraCloseBtn.classList.remove("active");
+    cameraUploadBtn.classList.remove("active");
+    cameraBtn.style.display = "block";
+    track.stop();
+});
 
 cameraBtn.addEventListener("click", function () {
     cameraBtn.style.display = "none";
@@ -97,21 +144,4 @@ cameraTrigger.addEventListener("click", function () {
         name: inputName.value,
         image: cameraOutput.src
     });
-});
-
-socket.on('image', (data) => {
-    let li = document.createElement('li');
-    let img = document.createElement('img');
-    img.src = data.image;
-    li.appendChild(img);
-    li.innerHTML += data.name;
-
-    // Check if the message is sent by the user
-    if (data.name === inputName.value) {
-        li.classList.add('current-user');
-    }
-
-    messages.appendChild(li);
-    typingState.innerHTML = "";
-    messages.scrollTop = messages.scrollHeight
 });
